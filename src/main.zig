17deab9826d4b247e3a7e7300d1320990b9c47cc
apple-x-co <apple.x.co@gmail.com>
2023-01-21 20:56:38 +0900

    chore: ci

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    
+
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
     // 引数
