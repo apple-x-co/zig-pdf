4355b37895b942378119065243a8f453fe6f959a
apple-x-co <apple.x.co@gmail.com>
2023-01-28 18:07:37 +0900

    chore: add Alignment

---
+++ src/Pdf/Alignment.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub const Alignment = enum {
+    bottom_center,
+    bottom_left,
+    bottom_right,
+    center,
+    center_left,
+    center_right,
+    top_center,
+    top_left,
+    top_right,
+};
