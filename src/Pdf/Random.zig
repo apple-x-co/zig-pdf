75baf02426e124689decd0919296cc2ad9509661
apple-x-co <apple.x.co@gmail.com>
2023-02-20 10:30:04 +0900

    fix: Random.id

--- src/Pdf/Random.zig
+++ src/Pdf/Random.zig
@@ -1,7 +1,16 @@
 const std = @import("std");
 
 pub fn generate(comptime T: type) T {
-    var rand = std.rand.DefaultPrng.init(@intCast(u64, std.time.milliTimestamp()));
+    const static = struct {
+        var rand = std.rand.DefaultPrng.init(0);
+    };
 
-    return rand.random().int(T);
+    return static.rand.random().int(T);
+}
+
+test {
+    const one = generate(u32);
+    const two = generate(u32);
+
+    try std.testing.expect(one != two);
 }
